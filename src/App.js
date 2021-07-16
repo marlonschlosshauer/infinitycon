9ac5ea98b63bb5af100a4d68663b5c4bdc3eadb8
marlonschlosshauer <marlonschlosshauer@gmail.com>
2021-07-16 19:37:35 +0200

    Infinite scroll

--- src/App.js
+++ src/App.js
@@ -2,19 +2,38 @@ import './App.css';
 import * as Identicon from "identicon.js";
 import { v4 as uuid } from 'uuid';
 
-function Icon(props) {
-	const data = new Identicon(props.id, 420).toString();
-	return (<img className='identicon' src={`data:image/png;base64,${data}`} />)
+import { useState, useEffect, useRef } from 'react';
+
+const Icon = (props) => {
+	return (<img className='identicon' src={`data:image/png;base64,${new Identicon(props.id, 420).toString()}`} />)
 }
 
-function Generator(size = 25) {
+const Generator = (size = 25) => {
 	return new Array(size).fill().map((id, index) => (<Icon id={uuid()} key={index} />));
 }
 
-function App() {
+const isBottom = (ref) => ref.current && ref.current.getBoundingClientRect().bottom <= (window.innerHeight * 1.7);
+
+const Infinite = ({ onBottomHit }) => {
+	const contentRef = useRef(null);
+
+	useEffect(() => {
+		onBottomHit();
+		document.addEventListener('scroll', () => {
+			if (isBottom(contentRef)) {
+				onBottomHit();
+			};
+		});
+	}, []);
+	return <div ref={contentRef}></div>;
+}
+
+const App = () => {
+	const [icons, setIcons] = useState([]);
 	return (
 		<div className="App">
-			{Generator()}
+			{icons}
+			<Infinite onBottomHit={() => setIcons(i => ([...i, Generator()]))} />
 		</div>
 	);
 }
